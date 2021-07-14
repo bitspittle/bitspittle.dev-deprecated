2451fa1b0dbc56b2ce1eb40779fccbf199fe2b2a
David Herman <bit.spittle@gmail.com>
2021-07-13 17:00:16 -0700

    Add Chakra-UI dependency

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,15 @@
 import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type {AppProps} from 'next/app'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+// See https://chakra-ui.com/
+import {ChakraProvider} from "@chakra-ui/react"
+
+function MyApp({Component, pageProps}: AppProps) {
+    return (
+        <ChakraProvider>
+            <Component {...pageProps} />
+        </ChakraProvider>
+    )
 }
+
 export default MyApp
