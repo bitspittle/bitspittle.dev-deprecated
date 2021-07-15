caa1d685f4099034e0ada8333481394feaa0c0ff
David Herman <bit.spittle@gmail.com>
2021-07-15 10:41:18 -0700

    Initial hook for theming

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,13 @@
 import '../styles/globals.css'
 import type {AppProps} from 'next/app'
+import theme from "./theme";
 
 // See https://chakra-ui.com/
 import {ChakraProvider} from "@chakra-ui/react"
 
 function MyApp({Component, pageProps}: AppProps) {
     return (
-        <ChakraProvider>
+        <ChakraProvider theme={theme}>
             <Component {...pageProps} />
         </ChakraProvider>
     )
